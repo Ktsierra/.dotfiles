19d8db2dc15682e098b035eb6c7b66b7bbe30695
Ktsierra <josejaviersierra1998@gmail.com>
2025-07-01 14:47:48 -0400

    Enhance directory selection in tmux sessionizer by including dotfiles and home directory options

--- .config/tmux/tmux-sessionizer.sh
+++ .config/tmux/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/Projects/Personal ~/Projects/Work -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$( ( find ~/Projects/Personal ~/Projects/Work -mindepth 1 -maxdepth 1 -type d; echo ~/.dotfiles; echo ~) | fzf)
 fi
 
 if [[ -z $selected ]]; then
