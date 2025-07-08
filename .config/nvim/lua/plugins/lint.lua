2bbb8c886710815828effcb444d08ec5b586853b
Ktsierra <josejaviersierra1998@gmail.com>
2025-07-08 13:12:03 -0400

    feat: Add Neovim configuration and plugins
    - Introduced a comprehensive Neovim configuration in `opt.lua` with various settings for better usability, including line numbers, mouse support, clipboard synchronization, and more.
    - Added `nvim-autopairs` for automatic pairing of brackets and quotes.
    - Integrated `blink.cmp` for enhanced completion experience with LSP and snippets.
    - Implemented `conform.nvim` for automatic code formatting on save.
    - Configured `nvim-dap` for debugging support, including key mappings and UI setup.
    - Enhanced Git integration with `gitsigns.nvim` for displaying git changes in the gutter.
    - Added `guess-indent.nvim` for automatic detection of indentation settings.
    - Included `indent-blankline.nvim` for visual indentation guides.
    - Set up `nvim-lint` for linting support across various file types.
    - Integrated `mini.nvim` for small utility plugins, including improved text object handling and status line.
    - Configured `neo-tree.nvim` for file system navigation.
    - Added `oil.nvim` for enhanced file management.
    - Integrated `telescope.nvim` for fuzzy finding capabilities.
    - Included `todo-comments.nvim` for highlighting TODOs and notes in comments.
    - Configured `nvim-treesitter` for improved syntax highlighting and code navigation.
    - Added `which-key.nvim` for displaying key mappings.
    - Introduced `eldritch.nvim` and `tokyonight.nvim` themes for enhanced aesthetics.

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -6,12 +6,12 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        markdown = { 'markdownlint-cli2' },
         -- To use the linter eslint must be installed in the project using pnpm i eslint
-        javascript = { 'eslint' },
-        typescript = { 'eslint' },
-        javascriptreact = { 'eslint' },
-        typescriptreact = { 'eslint' },
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
+        javascriptreact = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
