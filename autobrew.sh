5b7feabc215e0c0cd14b2e7b67702bef7f754edb
Ktsierra <josejaviersierra1998@gmail.com>
2025-07-30 13:51:16 -0400

    ffmpeg added

--- autobrew.sh
+++ autobrew.sh
@@ -302,7 +302,7 @@ main() {
     # Customise the following list variables (tap_list, term_list and cask_list) 
     # Leave list blank or comment out the list if not required.
     tap_list="nikitabobko/tap sst/tap"
-    term_list="node neovim tmux git gh awscli vercel-cli pnpm npm yarn nvm stow watchman starship fzf eza bat ripgrep zsh-autosuggestions zoxide opencode gemini-cli gcc make fd luarocks wget go lazygit imagemagick pkgconfig tree-sitter typescript"
+    term_list="node neovim tmux git gh awscli vercel-cli pnpm npm yarn nvm stow watchman starship fzf eza bat ripgrep zsh-autosuggestions zoxide opencode gemini-cli gcc make fd luarocks wget go lazygit imagemagick pkgconfig tree-sitter typescript ffmpeg"
     cask_list="font-jetbrains-mono-nerd-font karabiner-elements ghostty nikitabobko/tap/aerospace gimp zulu@17 firefox google-chrome lm-studio"
     clear
     term_colors
