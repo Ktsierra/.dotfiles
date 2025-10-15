94ea20fa7211d469a55c9358caa2305c6f5add5c
Ktsierra <josejaviersierra1998@gmail.com>
2025-10-15 18:51:24 -0400

    fix mini new repo

--- .config/nvim/lua/plugins/ai/copilot.lua
+++ .config/nvim/lua/plugins/ai/copilot.lua
@@ -31,7 +31,7 @@ return {
       'github/copilot.vim',
       -- Diff looks
       {
-        'mini-nvim/mini.diff',
+        'nvim-mini/mini.diff',
         config = function()
           local diff = require 'mini.diff'
           diff.setup {
