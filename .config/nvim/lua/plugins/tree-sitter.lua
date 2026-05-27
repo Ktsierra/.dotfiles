7a04e08de2b6ab0534e7de4a6649f03e5f6fe00a
Ktsierra <josejaviersierra1998@gmail.com>
2026-05-27 10:25:13 -0400

    feat(nvim/treesitter): add tsx/jsx support and generic FileType start
    Register tsx/jsx for the react filetypes and start treesitter from the
    buffer's resolved language in a single FileType autocmd.

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -16,14 +16,21 @@ return {
       'query',
       'vim',
       'vimdoc',
-      'javascript',
       'typescript',
+      'javascript',
+      'tsx',
+      'jsx',
     }
+    vim.treesitter.language.register('tsx', 'typescriptreact')
+    vim.treesitter.language.register('jsx', 'javascriptreact')
     require('nvim-treesitter').install(filetypes)
     vim.api.nvim_create_autocmd('FileType', {
-      pattern = filetypes,
-      callback = function()
-        vim.treesitter.start()
+      callback = function(args)
+        local buf, filetype = args.buf, args.match
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then return end
+        if not vim.treesitter.language.add(language) then return end
+        vim.treesitter.start(buf, language)
         vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         vim.wo.foldmethod = 'expr'
